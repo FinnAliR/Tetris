bad0c75f82d4ace3cd1aaa66b29cdb88d1833f89
FinnAliR <120427923+FinnAliR@users.noreply.github.com>
2024-03-20 03:41:10 +0000

    Made a display

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1080, 1280))
 clock = pygame.time.Clock()
 running = True
 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running= False
+    screen.fill("gray")
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit
